5e3eef70bb2801c40487114bef5da4e63f284fe9
bargav <bhar1red@gmail.com>
2016-06-13 11:00:32 -0400

    sign up and renew routes generated

--- app/router.js
+++ app/router.js
@@ -6,6 +6,17 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('sign-up', function(){
+    this.route('personal-information');
+    this.route('background-information');
+    this.route('payment-information');
+  });
+
+  this.route('renew'function(){
+    this.route('personal-information');
+    this.route('background-information');
+    this.route('payment-information');
+  });
 });
 
 export default Router;
