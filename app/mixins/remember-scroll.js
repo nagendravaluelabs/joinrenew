5fd88e5ff31c66c6ad76b54ee1a685eecb1df686
Nagendra Babu <nagendra.dasari@SHVLSP1391F1D.SLS.ads.valuelabs.net>
2016-06-29 16:21:30 +0530

    DEV-3405: Issue: 26 - Removed Scroll to top Timing

--- app/mixins/remember-scroll.js
+++ app/mixins/remember-scroll.js
@@ -4,10 +4,10 @@ export default Ember.Mixin.create({
   scrollToTop: function() {
     var scrollTop = jQuery(window).scrollTop();
     if( scrollTop > 10 ) {
-      jQuery("html, body").animate({"scrollTop": "0px"}, 500);
+      jQuery("html, body").animate({"scrollTop": "0px"}, 0);
     }
     jQuery("html, body").on("scroll mousedown wheel DOMMouseScroll mousewheel keyup touchmove", function(){
-      jQuery("html, body").stop();
+      jQuery("html, body").stop(true, true);
     });
   }
 });
