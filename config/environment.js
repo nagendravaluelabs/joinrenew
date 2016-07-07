b22851c02a3455baba768801a26d133129adb6d5
Nagendra Babu <nagendra.dasari@SHVLSP1391F1D.SLS.ads.valuelabs.net>
2016-07-07 09:35:46 +0530

    Updated Build name from Workspace to aia-joinrenew, Added SASS for Join Homepage & Renew Homepage

--- config/environment.js
+++ config/environment.js
@@ -2,7 +2,7 @@
 
 module.exports = function(environment) {
   var ENV = {
-    modulePrefix: 'workspace',
+    modulePrefix: 'aia-joinrenew',
     environment: environment,
     baseURL: '/',
     locationType: 'auto',
