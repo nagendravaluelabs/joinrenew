adc1180b665b3371a6315d97bf59827b324d1e05
murali.kakarla <murali.kakarla@valuelabs.com>
2016-07-15 13:15:47 +0530

    DEV-3375 SASS changes merged

--- app/routes/primary-information.js
+++ app/routes/primary-information.js
@@ -2,14 +2,11 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   activate() {
-    this.controllerFor("application").set("model.class","no-sidebars page-primary-info");
+    this.controllerFor("application").set("model.class","no-sidebars page-primary-info personal-info-component");
   },
   model() {
-    var primaryInfo = Ember.$.getJSON("https://aia-netforum-ws-sgsjohnny.c9users.io/web/getdata.php?datatype=user&key=1234").then(function(data){
-      return data;
-    })
     return Ember.RSVP.hash({
-      primaryInformationData : primaryInfo,
+      generic : this.store.findAll("primary-information"),
       steps: [
         {
           "title": "Step 1. Primary information",
@@ -61,6 +58,13 @@ export default Ember.Route.extend({
           }
         ],
         "primaryInformation": {
+          "personalInfo" : {
+            "prefix": "48baa5c1-cb00-4996-930f-82967b81c8a7",
+            "firstName": "Thomas",
+            "middleName": "Y",
+            "lastName": "sds",
+            "suffix": "II"
+          },
           "contactInfo" : {
             "addressType": "home",
             "homeCountry": "UNITED STATES",
