d79ec04056bd70a21825755cdd340da47e3e6b68
Valiveti Ramakrishna <ramakrishna.valiveti@SHVLSP2344F1D.SLS.ads.valuelabs.net>
2016-07-15 18:33:17 +0530

    Added User-Data to Personal and contact info

--- app/controllers/primary-information.js
+++ app/controllers/primary-information.js
@@ -7,64 +7,68 @@ export default Ember.Controller.extend({
     editContactInfo: false,
     primaryData: Ember.inject.service('user-data'),
     userData: [],
+    init: function() {
+      this.userDataFunc();
+    },
     userDataObserver: function() {
-      var primaryData, personalData, name, address, userData;
+      this.userDataFunc();
+    }.observes('primaryData.data'),
+    userDataFunc: function() {
+      var primaryData, personalData, name, primaryAddress, userData;
       userData = [];
       primaryData = this.get("primaryData");
-      personalData = primaryData.data.personal;
-      name = personalData.prefix + " " + personalData.firstname + " " + personalData.middlename + " " + personalData.lastname + " " + personalData.suffix;
-      var index = userData.length;
-      userData[index] = {};
-      userData[index] = {"title": "", "value": name, "class": "full-name"};
-      if(typeof personalData.phone != "undefined") {
-        if(typeof personalData.phone.home != "undefined" && personalData.phone.home.value!="") {
-          index++;
-          userData[index] = {};
-          userData[index] = {"title": "Home phone", "value": personalData.phone.home.value, "class": "home-phone"};
-          if(personalData.phone.primary === "home") {
-            userData[index].isPrimary = true;
-          }
-        }
-        if(typeof personalData.phone.mobile !="undefined" && personalData.phone.mobile.value!="") {
-          index++;
-          userData[index] = {};
-          userData[index] = {"title": "Mobile phone", "value": personalData.phone.mobile.value, "class": "mobile-phone"};
-          if(personalData.phone.primary === "mobile") {
-            userData[index].isPrimary = true;
-          }
-        }
-        if(typeof personalData.phone.work != "undefined" && personalData.phone.work.value!="") {
-          index++;
-          userData[index] = {};
-          userData[index] = {"title": "Work phone", "value": personalData.phone.work.value, "class": "work-phone"};
-          if(personalData.phone.primary === "work") {
-            userData[index].isPrimary = true;
-          }
-        }
-      }
-      
-      if(typeof personalData.address != "undefined") {
-        index++;
+      if(primaryData.data != "undefined" && primaryData.data != "") {
+        personalData = primaryData.data.personal;
+        name = personalData.prefix + " " + personalData.firstname + " " + personalData.middlename + " " + personalData.lastname + " " + personalData.suffix;
+        var index = userData.length;
         userData[index] = {};
-        if(personalData.address.primary == "home") {
-          address += personalData.address.home.line1+"\n";
-          address += personalData.address.home.line2+"\n";
-          address += personalData.address.home.city + ", ";
-          address += personalData.address.home.state.value + ", ";
-          address += personalData.address.home.country.value + ", ";
-          address += personalData.address.home.zip;
-        } else {
-          address += personalData.address.work.line1+"\n";
-          address += personalData.address.work.line2+"\n";
-          address += personalData.address.work.city + ", ";
-          address += personalData.address.work.state.value + ", ";
-          address += personalData.address.work.country.value + ", ";
-          address += personalData.address.work.zip;
+        userData[index] = {"title": "", "value": name, "class": "full-name"};
+        if(typeof personalData.phone != "undefined") {
+          if(typeof personalData.phone.home != "undefined" && personalData.phone.home.value!="") {
+            index++;
+            userData[index] = {};
+            userData[index] = {"title": "Home phone", "value": personalData.phone.home.value, "class": "home-phone"};
+            if(personalData.phone.primary === "home") {
+              userData[index].isPrimary = true;
+            }
+          }
+          if(typeof personalData.phone.mobile !="undefined" && personalData.phone.mobile.value!="") {
+            index++;
+            userData[index] = {};
+            userData[index] = {"title": "Mobile phone", "value": personalData.phone.mobile.value, "class": "mobile-phone"};
+            if(personalData.phone.primary === "mobile") {
+              userData[index].isPrimary = true;
+            }
+          }
+          if(typeof personalData.phone.work != "undefined" && personalData.phone.work.value!="") {
+            index++;
+            userData[index] = {};
+            userData[index] = {"title": "Work phone", "value": personalData.phone.work.value, "class": "work-phone"};
+            if(personalData.phone.primary === "work") {
+              userData[index].isPrimary = true;
+            }
+          }
         }
-        userData[index] = {"title": "address", "value": personalData.phone.work.value, "class": "address"};
+        
+        if(typeof personalData.address != "undefined") {
+          index++;
+          userData[index] = {};
+          primaryAddress=[];
+          if(personalData.address.primary == "home") {
+            primaryAddress[0]= personalData.address.home.line1;
+            primaryAddress[1]= personalData.address.home.line2;
+            primaryAddress[2]= personalData.address.home.city + ", " + personalData.address.home.state.value + ", " + personalData.address.home.country.value + ", " + personalData.address.home.zip;
+          } else {
+            primaryAddress[0]= personalData.address.work.line1;
+            primaryAddress[1]= personalData.address.work.line2;
+            primaryAddress[2]= personalData.address.work.city + ", " + personalData.address.work.state.value + ", " + personalData.address.work.country.value + ", " + personalData.address.work.zip;
+          }
+          console.log(primaryAddress);
+          userData[index] = {"title": "address", "value": primaryAddress, "class": "address"};
+        }
+        this.set("userData", userData);
       }
-      this.set("userData", userData);
-    }.observes('primaryData.data'),
+    },
     actions: {
         showPersonalInfo: function () {
             'use strict';
