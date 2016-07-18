090c280b9f5d2e2fd16c0be633a967e4d199a756
murali.kakarla <murali.kakarla@valuelabs.com>
2016-07-18 19:14:57 +0530

    DEV-3375: Thank you page designed and integrated

--- app/router.js
+++ app/router.js
@@ -16,7 +16,8 @@ Router.map(function() {
   this.route('renew-verify-membership',{ path: '/renew/renew-verify-membership' });
   this.route('primary-information', {path: '/renew/primary-information' });
   this.route('membership-dues', {path: '/renew/membership-dues' });
-  this.route('payment-information', {path: '/renew/payment-information' });  
+  this.route('payment-information', {path: '/renew/payment-information' }); 
+  this.route('thankyou-page', {path: '/renew/thankyou-page' });    
   this.route('page-not-found', { path: '/*path' });
 });
 
