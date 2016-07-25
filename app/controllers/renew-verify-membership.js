555d0042261551f18b5191a3e25b0435e0735457
murali.kakarla <murali.kakarla@valuelabs.com>
2016-07-25 17:27:13 +0530

    DEV-3364: Fixed client side code for Remove pURLs for Lapsed Members' Invoices (Verification Questionnaire Page)
    and dynamic link added for mail in your renewal  as per dev-3459

--- app/controllers/renew-verify-membership.js
+++ app/controllers/renew-verify-membership.js
@@ -2,7 +2,7 @@ import DS from 'ember-data';
 
 export default Ember.Controller.extend({
   addressData: Ember.inject.service('user-data'),
-  address: "",
+  address: "",  
   addressObserver: function(){
     var addressData = this.get("addressData");
     if(typeof addressData.data != "undefined") {
@@ -12,7 +12,15 @@ export default Ember.Controller.extend({
         this.set("address", addressData.data.chapter.state);
       } else if(typeof addressData.data.chapter.national) {
         this.set("address", addressData.data.chapter.national);
-      }
-    }
+      }	
+	  
+	  if(addressData.data.membership){
+		  this.set("renewMailLink","https://aia.hbp.com/assets/pdf/3"+addressData.data.membership.memberid);		  
+	  }else{
+		  
+	  }
+	  if(addressData.data.membership.membershipstatus == 'Terminated')
+	    this.set("dipslayFlag", "display:none");  
+    }	
   }.observes("addressData.data")
 });
