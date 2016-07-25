a2f60126e4ad2b8a5585c73a59a352e0ee00291a
murali.kakarla <murali.kakarla@valuelabs.com>
2016-07-25 17:51:15 +0530

    DEV-3459: dev:1588 handled at client side.

--- app/controllers/renew-verify-membership.js
+++ app/controllers/renew-verify-membership.js
@@ -12,7 +12,19 @@ export default Ember.Controller.extend({
         this.set("address", addressData.data.chapter.state);
       } else if(typeof addressData.data.chapter.national) {
         this.set("address", addressData.data.chapter.national);
-      }	
+      }	else{	  
+		   addressData.data.chapter["defaultInfo"]={
+            "chapterlevel": "default",
+            "chaptername": "",
+            "tax_deductible": "",
+            "address": "The American Institute of Architects 1735 New York Ave NwWashington, DC 20006-5209",
+            "phone": "",
+            "email": "",
+            "website": "www.aia.org"
+            };			  	   
+		    this.set("address",addressData.data.chapter.defaultInfo);	 
+		  		  
+	  }
 	  
 	  if(addressData.data.membership){
 		  this.set("renewMailLink","https://aia.hbp.com/assets/pdf/3"+addressData.data.membership.memberid);		  
