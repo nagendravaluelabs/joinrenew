b72aa2f694423a55610784aabe7e30ba7f433b90
murali.kakarla <murali.kakarla@valuelabs.com>
2016-08-01 18:47:26 +0530

    Added Ember.$ instead of $

--- app/components/select-state.js
+++ app/components/select-state.js
@@ -5,7 +5,7 @@ export default Ember.Component.extend({
     tagName: '',
     didUpdate: function() {
       setTimeout(function (){
-        $(".select-chosen").trigger("chosen:updated");
+        Ember.$(".select-chosen").trigger("chosen:updated");
       },100);
     }
 });
