e1c8ec8923cab880c81d42dbc63f36d8db71b612
Wasim Sheikh <wasim.sheikh@valuelabs.com>
2016-08-09 14:27:52 +0530

    UI Design Archipac completed

---
+++ app/components/archipac-donation.js
@@ -0,0 +1,8 @@
+/*jslint white:true, devel:true, es6:true, this:true, browser:true*/
+/*global jQuery*/
+/*global $*/
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+    tagName: ""
+});
