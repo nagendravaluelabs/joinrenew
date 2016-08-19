26e44ae4b3e6fc35423d6a4ded23cde884324841
Nagendra Babu <nagendra.dasari@SHVLSP1391F1D.SLS.ads.valuelabs.net>
2016-08-19 20:15:24 +0530

    DEV-3502: updated design for thank you  / complete page

--- app/controllers/complete.js
+++ app/controllers/complete.js
@@ -1,7 +1,6 @@
 /*jslint white:true, devel:true, es6:true, this:true, browser:true */
 import Ember from "ember";
 import rememberScroll from "../mixins/remember-scroll";
-const {$} = Ember;
 export default Ember.Controller.extend(rememberScroll, {
     init: function () {
         "use strict";
