f62635026b767d6ce930fc3d60de4870c56b34dd
Suneel Kumar <suneel.nunna@valuelabs.com>
2016-08-23 00:27:21 +0530

    Removed Ember test errors.

--- app/components/archipac-donation.js
+++ app/components/archipac-donation.js
@@ -1,10 +1,11 @@
+/*jslint white:true, devel:true, es6:true, this:true, browser:true*/
+/*global $*/
 import Ember from 'ember';
 
 export default Ember.Component.extend({
     /* tagName: "" */
     actions :{
     showArchipacDis: function () {
-      console.log("clicked disclaimer");
       "use strict";
       $('#archipac_disclaiminfo').dialog('open');
       return false;
