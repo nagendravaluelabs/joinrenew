a1cd3c0f9f347a3d6e63e74ca9cba60db863497b
Suneel Kumar <suneel.nunna@valuelabs.com>
2016-08-23 19:25:30 +0530

    DEV - 4297 - Display ArchiPAC Disclaimer Modal in Payment Page - Fixed.

--- app/components/archipac-donation.js
+++ app/components/archipac-donation.js
@@ -1,9 +1,31 @@
 /*jslint white:true, devel:true, es6:true, this:true, browser:true*/
 /*global $*/
 import Ember from 'ember';
-
 export default Ember.Component.extend({
-    actions :{
+  didInsertElement() {
+    "use strict";
+    this._super(...arguments);
+    $('#archipac_disclaiminfo').dialog({
+      modal: true,
+      autoOpen: false,
+      draggable: false,
+      resizable: false,
+      width: $(window).width() > 850 ? 850 : '90%',
+      title: 'ArchiPAC disclaimer',
+      responsive: true,
+      closeText: '',
+      appendTo: "#main-content.page-sign-up-payment #archipac_dis_modal", 
+      show: false,
+      hide: false
+    });
+    $(window).resize(function () {
+      $("#archipac_disclaiminfo").dialog("option", "width", $(window).width() > 850 ? 850 : '90%');
+    });
+    $('body').on("click", ".ui-widget-overlay", function () {
+      $("#archipac_disclaiminfo").dialog("close");
+    }); 
+  },
+  actions :{
     showArchipacDis: function () {
       "use strict";
       $('#archipac_disclaiminfo').dialog('open');
--- app/routes/payment-information.js
+++ app/routes/payment-information.js
@@ -3,7 +3,6 @@
 /*global window*/
 
 import Ember from 'ember';
-
 export default Ember.Route.extend({
     activate() {
       "use strict";
@@ -34,25 +33,6 @@ export default Ember.Route.extend({
         });
         $('body').on("click", ".ui-widget-overlay", function () {
           $("#paymentplan_extrainfo").dialog("close");
-        });
-        $('#archipac_disclaiminfo').dialog({
-          modal: true,
-          autoOpen: false,
-          draggable: false,
-          resizable: false,
-          width: $(window).width() > 850 ? 850 : '90%',
-          title: 'ArchiPAC disclaimer',
-          responsive: true,
-          closeText: '',
-          appendTo: "#main-content.page-sign-up-payment #archipac_dis_modal", 
-          show: false,
-          hide: false
-        });
-        $(window).resize(function () {
-          $("#archipac_disclaiminfo").dialog("option", "width", $(window).width() > 850 ? 850 : '90%');
-        });
-        $('body').on("click", ".ui-widget-overlay", function () {
-          $("#archipac_disclaiminfo").dialog("close");
         });
       });
   },
