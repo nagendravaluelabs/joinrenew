594e5a35621f68600b56d77b6496279f9ec89d0a
Nagendra <nagender0619@gmail.com>
2016-09-23 18:11:11 +0530

    DEV-3374: Added Environment Config implementation for Staging and Development

--- config/environment.js
+++ config/environment.js
@@ -10,6 +10,17 @@ module.exports = function(environment) {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
         // e.g. 'with-controller': true
+      },
+      JANRAIN: {
+        AIA_API_CLIENT_ID: process.env.AIA_API_CLIENT_ID || "jhxbmd679pv6j3hatb3yz7xdpgtauced",
+        AIA_API_SECRET: process.env.AIA_API_SECRET || "mx3zd27mcvdbkrmnr829cczcvnnr62dq",
+        AIA_APP_ID: process.env.AIA_APP_ID || "6u2cx5favu7vq3tgc892r67rey",
+        AIA_CAPTURE_SERVER: process.env.AIA_CAPTURE_SERVER || "https://american-institute-of-architects-dev.us-dev.janraincapture.com",
+        AIA_APP_URL: process.env.AIA_APP_URL || "https://american-institute-of-architects-dev.rpxnow.com",
+        AIA_HTTP_URL: process.env.AIA_HTTP_URL || "http://d29usylhdk1xyu.cloudfront.net/load/american-institute-of-architects-dev",
+        AIA_HTTPS_URL: process.env.AIA_HTTPS_URL || "https://d29usylhdk1xyu.cloudfront.net/load/american-institute-of-architects-dev",
+        AIA_FEDERATE_SERVER: process.env.AIA_FEDERATE_SERVER || "https://american-institute-of-architects-dev.us.janrainsso.com",
+        AIA_ENVIRONMENT: environment
       }
     },
 
@@ -18,17 +29,16 @@ module.exports = function(environment) {
       // when it is created
     },
     AIA_NEXT_YEAR: "2017",
-    AIA_CORS: process.env.AIA_CORS || "//52.1.198.224/",
-    AIA_API_CLIENT_ID: process.env.AIA_API_CLIENT_ID || 'jhxbmd679pv6j3hatb3yz7xdpgtauced',
-    AIA_API_SECRET: process.env.AIA_API_SECRET || 'mx3zd27mcvdbkrmnr829cczcvnnr62dq'
+    AIA_CORS: process.env.AIA_CORS || "//52.1.198.224/"
   };
 
   ENV.AIA_SAVE_URL= process.env.AIA_SAVE_URL ? ENV.AIA_CORS+process.env.AIA_SAVE_URL : ENV.AIA_CORS+'http://kbsjoinrenewsu7uhssau2.devcloud.acquia-sites.com/renewjson/update';
-  
   ENV.AIA_DRUPAL_URL= (process.env.AIA_DRUPAL_URL) ? ENV.AIA_CORS+process.env.AIA_DRUPAL_URL : ENV.AIA_CORS+'http://kbsjoinrenewsu7uhssau2.devcloud.acquia-sites.com/renewjson/getdata';
-  ENV.AIA_API_URL= ENV.AIA_CORS+'https://american-institute-of-architects-dev.us-dev.janraincapture.com';
+  ENV.AIA_API_URL= ENV.AIA_CORS+ENV.EmberENV.JANRAIN.AIA_CAPTURE_SERVER;
   ENV.AIA_EMPLOYMENT_LOOKUP_URL= ENV.AIA_CORS+'http://aia-recommend-staging.us-east-1.elasticbeanstalk.com/organizations';
-
+  
+  ENV.AIA_API_CLIENT_ID= ENV.EmberENV.JANRAIN.AIA_API_CLIENT_ID;
+  ENV.AIA_API_SECRET= ENV.EmberENV.JANRAIN.AIA_API_SECRET;
   if (environment === 'development') {
     // ENV.APP.LOG_RESOLVER = true;
     // ENV.APP.LOG_ACTIVE_GENERATION = true;
