d5461ca50c56ffe9e0696ad7cf7c9a104edf8771
Bargav Kondapu <bargavkondapu@Bargavs-MacBook-Pro.local>
2016-10-11 12:50:36 -0400

    production configuration setup

--- config/deploy.js
+++ config/deploy.js
@@ -60,7 +60,7 @@ module.exports = function(deployTarget) {
     }
     if (!process.env.AIA_HTTPS_URL) {
       process.env.AIA_HTTPS_URL = process.env.DEV_JANRAIN_API_HTTPS_URL;
-    }  
+    }
     if (!process.env.AIA_FEDERATE_SERVER) {
       process.env.AIA_FEDERATE_SERVER = process.env.DEV_JANRAIN_API_FEDERATE_SERVER;
     }
@@ -117,7 +117,7 @@ module.exports = function(deployTarget) {
     }
     if (!process.env.AIA_HTTPS_URL) {
       process.env.AIA_HTTPS_URL = process.env.STAGING_JANRAIN_API_HTTPS_URL;
-    }  
+    }
     if (!process.env.AIA_FEDERATE_SERVER) {
       process.env.AIA_FEDERATE_SERVER = process.env.STAGING_JANRAIN_API_FEDERATE_SERVER;
     }
@@ -142,6 +142,58 @@ module.exports = function(deployTarget) {
     ENV.build.environment = 'production';
     ENV['s3-index'].bucket = 'aia-joinrenew-prod';
     ENV['s3'].bucket = 'aia-joinrenew-prod';
+    if (!process.env.AIA_DRUPAL_URL) {
+      process.env.AIA_DRUPAL_URL = process.env.PROD_DRUPAL_URL;
+    }
+    if (!process.env.AIA_SAVE_URL) {
+      process.env.AIA_SAVE_URL = process.env.PROD_SAVE_URL;
+    }
+    if (!process.env.AIA_EMPLOYMENT_LOOKUP_URL) {
+      process.env.AIA_EMPLOYMENT_LOOKUP_URL = process.env.PROD_EMPLOYMENT_LOOKUP_URL;
+    }
+    if (!process.env.AIA_API_URL) {
+      process.env.AIA_API_URL = process.env.PROD_JANRAIN_API_URL;
+    }
+    if (!process.env.AIA_API_CLIENT_ID) {
+      process.env.AIA_API_CLIENT_ID = process.env.PROD_JANRAIN_API_CLIENT_ID;
+    }
+    if (!process.env.AIA_API_SECRET) {
+      process.env.AIA_API_SECRET = process.env.PROD_JANRAIN_API_CLIENT_SECRET;
+    }
+    if (!process.env.AIA_APP_ID) {
+      process.env.AIA_APP_ID = process.env.PROD_JANRAIN_APP_ID;
+    }
+    if (!process.env.AIA_CAPTURE_SERVER) {
+      process.env.AIA_CAPTURE_SERVER = process.env.PROD_JANRAIN_API_URL;
+    }
+    if (!process.env.AIA_APP_URL) {
+      process.env.AIA_APP_URL = process.env.PROD_JANRAIN_APP_URL;
+    }
+    if (!process.env.AIA_HTTP_URL) {
+      process.env.AIA_HTTP_URL = process.env.PROD_JANRAIN_API_HTTP_URL;
+    }
+    if (!process.env.AIA_HTTPS_URL) {
+      process.env.AIA_HTTPS_URL = process.env.PROD_JANRAIN_API_HTTPS_URL;
+    }
+    if (!process.env.AIA_FEDERATE_SERVER) {
+      process.env.AIA_FEDERATE_SERVER = process.env.PROD_JANRAIN_API_FEDERATE_SERVER;
+    }
+    if (!process.env.AIA_CORS) {
+      process.env.AIA_CORS = process.env.CORS_SERVER_URL;
+    }
+    if (!process.env.GTM_CODE) {
+      process.env.GTM_CODE = process.env.STAGING_GTM_CODE;
+    }
+  if (!process.env.CHAPTER_TRANSFER_FORM_LINK) {
+      process.env.CHAPTER_TRANSFER_FORM_LINK = process.env.STAGING_CHAPTER_TRANSFER_FORM_LINK;
+    }
+  if (!process.env.ASSOCIATE_TO_ARCHITECT_FORM_LINK) {
+      process.env.ASSOCIATE_TO_ARCHITECT_FORM_LINK = process.env.STAGING_ASSOCIATE_TO_ARCHITECT_FORM_LINK;
+    }
+  if (!process.env.MAIL_IN_YOUR_RENEWAL_LINK) {
+      process.env.MAIL_IN_YOUR_RENEWAL_LINK = process.env.STAGING_MAIL_IN_YOUR_RENEWAL_LINK;
+    }
+
   }
 
   // Note: if you need to build some configuration asynchronously, you can return
