0e1e1993ffad1ad15af383e4d67636b79d67a635
Nagendra <nagender0619@gmail.com>
2016-10-20 17:28:53 +0530

    Removed janrain secret from Ember APP

--- app/controllers/renew.js
+++ app/controllers/renew.js
@@ -19,9 +19,7 @@ export default Ember.Controller.extend({
               tokenParams = {
                 "redirect_uri": "http://localhost:4200/renew/?url_type=forgot",
                 "code": authCode,
-                "grant_type": "authorization_code",
-                "client_id": ENV.AIA_API_CLIENT_ID,
-                "client_secret": ENV.AIA_API_SECRET
+                "grant_type": "authorization_code"
               };
               tokenParams = $.param( tokenParams );
               tokenReqData = {
